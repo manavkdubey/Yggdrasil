6b48a9d0d6fffa056bf2fd5f33d9adfeb6e95c63
manavkdubey <dmanavkumar24@gmail.com>
2025-03-22 10:46:40 +0530

    feat: modify bind to make it deployable

--- src/main.rs
+++ src/main.rs
@@ -196,7 +196,7 @@ async fn main() -> std::io::Result<()> {
     env_logger::init();
 
     HttpServer::new(|| App::new().route("/ws", web::get().to(ws_handler)))
-        .bind("127.0.0.1:8080")?
+        .bind(("0.0.0.0", 8080))?
         .run()
         .await
 }
